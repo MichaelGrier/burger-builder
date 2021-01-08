d14875d139b675e161ac2c189937aa68cbcf3424
Michael Grier <michael.c.grier@gmail.com>
2021-01-08 09:42:53 -0700

    add comments to Scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,15 @@
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
+// Define environment
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+// Script will crash on unhandled rejections instead of silently ignoring them
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
+// Ensure environment variables are read
 require('../config/env');
 
 const path = require('path');
@@ -31,7 +29,7 @@ const measureFileSizesBeforeBuild =
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
 const useYarn = fs.existsSync(paths.yarnLockFile);
 
-// These sizes are pretty large. We'll warn for bundles exceeding them.
+// Warn for bundles exceeding these sizes
 const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 
@@ -40,16 +38,15 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
-// First, read the current file sizes in build directory.
-// This lets us display how much they changed later.
+// Read the current file sizes in build directory.
 measureFileSizesBeforeBuild(paths.appBuild)
   .then(previousFileSizes => {
     // Remove all content but keep the directory so that
     // if you're in it, you don't end up in Trash
     fs.emptyDirSync(paths.appBuild);
-    // Merge with the public folder
+    // Merge with public folder
     copyPublicFolder();
-    // Start the webpack build
+    // Start webpack build
     return build(previousFileSizes);
   })
   .then(
@@ -100,7 +97,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
     }
   );
 
-// Create the production build and print the deployment instructions.
+// Create the production build and print deployment instructions
 function build(previousFileSizes) {
   console.log('Creating an optimized production build...');
 
@@ -112,8 +109,7 @@ function build(previousFileSizes) {
       }
       const messages = formatWebpackMessages(stats.toJson({}, true));
       if (messages.errors.length) {
-        // Only keep the first error. Others are often indicative
-        // of the same problem, but confuse the reader with noise.
+        // Only keep the first error
         if (messages.errors.length > 1) {
           messages.errors.length = 1;
         }
