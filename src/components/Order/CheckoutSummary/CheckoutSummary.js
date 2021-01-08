2f308f2afcc3100947f336256b6e8b69837059cf
Michael Grier <michael.c.grier@gmail.com>
2021-01-08 09:15:30 -0700

    first commit

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h2 className={classes.header}>Almost there! Hit continue to enter an address</h2>
+            <div style={{width: '100%', margin: '0'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button 
+                btnType="Danger"
+                clicked={props.checkoutCancelled}>CANCEL</Button>
+            <Button 
+                btnType="Success"
+                clicked={props.checkoutContinued}>CONTINUE</Button>
+        </div>
+    );
+}
+
+export default checkoutSummary;
---
+++ src/containers/OrderConfirmation/OrderConfirmation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+import classes from './OrderConfirmation.css';
+import Button from '../../components/UI/Button/Button';
+
+const OrderConfirmation = () => {
+  return(
+    <div className={classes.OrderConfirmation}>
+      <h2 className={classes.Header}>Enjoy Your Meal!</h2>
+      <p style={{marginBottom: 0}}>Your order is on its way.</p>
+      <NavLink to="/"><Button btnType="Success">Home</Button></NavLink>
+    </div>
+  )
+}
+
+export default OrderConfirmation;
